53705e73c5f9e8520aa1105dac5a1df575f060a7
danny1210 <dldndgml1210@naver.com>
2024-09-27 18:50:01 +0900

    settings

---
+++ src/main/kotlin/com/example/dongguktoocean/DonggukToOceanApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dongguktoocean
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DonggukToOceanApplication
+
+fun main(args: Array<String>) {
+    runApplication<DonggukToOceanApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/dongguktoocean/DonggukToOceanApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dongguktoocean
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DonggukToOceanApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
